0a0cfc6d0dc041cbb697916ecaa5cc392279ccdd
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2021-11-20 22:42:53 +0400

    feat: added leaderboard

--- frontend/src/pages/Leaderboard/index.jsx
+++ frontend/src/pages/Leaderboard/index.jsx
@@ -1,9 +1,73 @@
 import React from 'react';
+import "./index.css";
 
 const Leaderboard = () => {
   return (
     <>
-      <h1 className="text-center text-white">We are in Leaderboard</h1>
+      <div className="leaderboard-page">
+        <table className="leaderboard">
+          <thead class="header"> 
+          <tr className="heading">
+            <th>Rank</th>
+            <th>Username</th>
+            <th>Percentage Match</th>
+          </tr>
+          </thead>
+
+          <tbody>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+            <tr className="names">
+              <td>21</td>
+              <td>Kadambari03</td>
+              <td>3300</td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </>
   );
 };
