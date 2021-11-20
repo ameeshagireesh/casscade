8578098ea8a4633dad0eec2a915c9b7227faad9a
sanjaybaskaran01 <72266283+sanjaybaskaran01@users.noreply.github.com>
2021-11-20 14:24:24 +0530

    feat: added rules content and home

--- frontend/src/components/navbar/index.jsx
+++ frontend/src/components/navbar/index.jsx
@@ -110,7 +110,7 @@ const Navbar = () => {
               className="bg-gray-900 text-white block px-3 py-2 rounded-md text-base font-medium"
               aria-current="page"
             >
-              Home
+             About
             </Link>
 
             <Link
--- frontend/src/pages/Rules/index.jsx
+++ frontend/src/pages/Rules/index.jsx
@@ -1,9 +1,44 @@
 import React from 'react';
- 
-const Rules = () =>{
-   return(
-      <h1 className="text-center text-white">We are in Rules</h1>
-   )
-}
+import './index.css';
+import listIcon from '../../assets/listicon.svg'
 
-export default Rules
+const Rules = () => {
+  return (
+    <div className="container mx-auto">
+      <p className="text-white">
+        <ul className="list" style={{listStyleImage:listIcon}}>
+          <li>
+            ROUND 1:
+            <br />
+            All participants will be provided a common static web page design to
+            be recreated within 30 minutes. Submissions will be judged based on
+            the accuracy of the recreation.
+          </li>
+          <br />
+          <li>
+            ROUND 2:
+            <br />
+            Participants will be split into groups, with a different design
+            being assigned to each group. Participants will compete only against
+            members of their respective groups. 1 hour will be provided for the
+            second round and criteria to determine the finalists will be the
+            time taken for submission, accuracy of the recreation, and
+            efficiency of the code.
+          </li>
+          <br />
+          <li>
+            ROUND 3:
+            <br />
+            In the final round, participants will be required to create an
+            individual website based on the theme ‘clean energy’ by 1:30 PM on
+            25th November 2021. Submissions may make use of boiler template
+            designs provided by CSI or be built from scratch by the participant.
+            <br />
+          </li>
+        </ul>
+      </p>
+    </div>
+  );
+};
+
+export default Rules;
