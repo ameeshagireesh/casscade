78cb518644162200686e717ae512f8790b183e3b
sanjaybaskaran01 <72266283+sanjaybaskaran01@users.noreply.github.com>
2021-11-21 15:52:39 +0530

    feat: rules styled

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -3,7 +3,7 @@ import svgReference from '../../assets/CaSScade.svg'
 
 const Home = () => {
   return (
-    <div className="flex flex-col">
+    <div className="">
     <img src={svgReference} />
     <p className="text-white">a CSS battle </p>
     <button className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4" style={{backgroundColor:"#410979"}}>
--- frontend/src/pages/Rules/index.jsx
+++ frontend/src/pages/Rules/index.jsx
@@ -1,39 +1,54 @@
 import React from 'react';
 import './index.css';
-import listIcon from '../../assets/listicon.svg'
+import listIcon from '../../assets/listicon.svg';
 
 const Rules = () => {
   return (
-    <div className="container mx-auto">
-      <p className="text-white">
-        <ul className="list" style={{listStyleImage:listIcon}}>
+    <div className="container mx-auto my-20 ">
+      <p className="text-white font-semibold text-xl ">
+        <ul>
           <li>
-            ROUND 1:
+            <img src={listIcon} className="inline-block" /> &nbsp;
+            <p className="inline" style={{ color: '#F1BD12' }}>
+              ROUND 1:
+            </p>
             <br />
-            All participants will be provided a common static web page design to
-            be recreated within 30 minutes. Submissions will be judged based on
-            the accuracy of the recreation.
+            <p style={{ paddingLeft: '2.5rem' }}>
+              All participants will be provided a common static web page design
+              to be recreated within 30 minutes. Submissions will be judged
+              based on the accuracy of the recreation.
+            </p>
           </li>
           <br />
           <li>
-            ROUND 2:
+            <img src={listIcon} className="inline-block" /> &nbsp;
+            <p className="inline" style={{ color: '#F1BD12' }}>
+              ROUND 2:
+            </p>
             <br />
-            Participants will be split into groups, with a different design
-            being assigned to each group. Participants will compete only against
-            members of their respective groups. 1 hour will be provided for the
-            second round and criteria to determine the finalists will be the
-            time taken for submission, accuracy of the recreation, and
-            efficiency of the code.
+            <p style={{ paddingLeft: '2.5rem' }}>
+              Participants will be split into groups, with a different design
+              being assigned to each group. Participants will compete only
+              against members of their respective groups. 1 hour will be
+              provided for the second round and criteria to determine the
+              finalists will be the time taken for submission, accuracy of the
+              recreation, and efficiency of the code.
+            </p>
           </li>
           <br />
           <li>
-            ROUND 3:
+            <img src={listIcon} className="inline-block" /> &nbsp;
+            <p className="inline" style={{ color: '#F1BD12' }}>
+              ROUND 3:
+            </p>
             <br />
-            In the final round, participants will be required to create an
-            individual website based on the theme ‘clean energy’ by 1:30 PM on
-            25th November 2021. Submissions may make use of boiler template
-            designs provided by CSI or be built from scratch by the participant.
-            <br />
+            <p style={{ paddingLeft: '2.5rem' }}>
+              In the final round, participants will be required to create an
+              individual website based on the theme ‘clean energy’ by 1:30 PM on
+              25th November 2021. Submissions may make use of boiler template
+              designs provided by CSI or be built from scratch by the
+              participant.
+            </p>
           </li>
         </ul>
       </p>
