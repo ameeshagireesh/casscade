aa343291fc3945cd1c32e7a5ff085c977eba1702
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2021-11-22 22:49:09 +0400

    fix: home page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,17 +1,22 @@
 import React from 'react';
 import casscadelogo from '../../assets/CaSScade.png';
 import casscadetext from '../../assets/text.png';
-import background from '../../assets/bg.png'
+import background from '../../assets/bg.png';
+import './index.css';
 
 const Home = () => {
   return (
     <div className="">
-    <img src={casscadelogo} classname="casscade-logo"/>
-    <img src={casscadetext} classname="casscade-logo"/>
-    <button className="bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4" style={{backgroundColor:"#410979"}}>
-      LOG IN
-      </button>
-    <img src={background} classname="casscade-bg"/>
+      <img src={casscadelogo} className="casscade-logo"/>
+      <img src={casscadetext} className="casscade-text"/>
+      <div className="login-position">
+        <button className="login" >
+          LOG IN
+        </button>
+      </div>
+      <div>
+        <img src={background} className="casscade-bg"/>
+      </div>
     </div>
   );
 };
