ffa3c90d34c3e261dff9071daf5f47694206d875
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2021-11-22 21:17:48 +0400

    fix: added components to home page

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
-import svgReference from '../../assets/CaSScade.svg'
+import casscadelogo from '../../assets/CaSScade.png';
+import casscadetext from '../../assets/text.png';
+import background from '../../assets/bg.png'
 
 const Home = () => {
   return (
     <div className="">
-    <img src={svgReference} />
-    <p className="text-white">a CSS battle </p>
-    <button className="bg-purple-500 hover:bg-blue-700 text-white font-bold py-2 px-4" style={{backgroundColor:"#410979"}}>
+    <img src={casscadelogo} classname="casscade-logo"/>
+    <img src={casscadetext} classname="casscade-logo"/>
+    <button className="bg-yellow-500 hover:bg-blue-700 text-white font-bold py-2 px-4" style={{backgroundColor:"#410979"}}>
       LOG IN
       </button>
+    <img src={background} classname="casscade-bg"/>
     </div>
   );
 };
