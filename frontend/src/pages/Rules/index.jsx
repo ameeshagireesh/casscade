96193edd1f13c718db1aa8fcfd60224d99ec2b3f
ameeshagireesh <ameesha.gireeshkm@gmail.com>
2021-11-23 23:29:52 +0400

    fix: made Rules responsive

--- frontend/src/pages/Rules/index.jsx
+++ frontend/src/pages/Rules/index.jsx
@@ -16,15 +16,16 @@ const Rules = () => {
             </p>
             <br />
             <p style={{ paddingLeft: '2.5rem' }}>
-            All participants will be a part of this round. The main objective of this round will be to visually replicate the target image 
-            using HTML/CSS as accurately as possible. You will be divided into groups and will be given 30 minutes to complete this round. 
-            The score will be calculated by taking into account two things: <br />
-                  How accurately you have replicated the image given? <br />
-                  The number of characters in the code <br />
-            
-            Better the match and the lesser the number of lines of code, the higher the score! <br />
-            
-            Platform: Discord <br />
+            All participants will be a part of this round. The main objective of this round will be to visually replicate the 
+            target image using HTML/CSS as accurately as possible. You will be divided into groups and will be given 30 minutes 
+            to complete this round, with 15 minutes to make submissions. The score will be calculated by taking into account two things: 
+              <ol className="sub-rules"> 
+                <li>1. How accurately you have replicated the image given </li>
+                <li> 2. Number of characters in the code. </li>
+              </ol>
+            <p style={{ paddingTop: '1.5rem'}}>
+            Better the match and lesser the number of lines of code, higher the score. <br />
+            Platform: -- Discord <br /> </p>
             </p>
           </li>
           <br />
@@ -35,12 +36,12 @@ const Rules = () => {
             </p>
             <br />
             <p style={{ paddingLeft: '2.5rem' }}>
-            The people who passed the previous round will participate in this round. The participants will be divided into groups and 
-            each group will compete amongst themselves. The objective of this round will be to replicate the target image using HTML/CSS. 
-            The round participants will be judged on time taken, the efficiency of code, and recreation accuracy. The duration of this round 
-            will be 1 hour. <br />
-            
-            Platform: Discord <br />
+            The people who passed the previous round will participate in this round. The participants will be divided 
+            into groups and each group will compete amongst themselves. The objective of this round will be to replicate 
+            the target image using HTML/CSS.  The round participants will be judged on time taken, efficiency of code, and 
+            recreation accuracy. The duration of this round will be 30 minutes as well, with 15 minutes to make submissions. <br />
+            <p style={{ paddingTop: '1.5rem'}}>
+            Platform: -- Discord <br /> </p>
             </p>
           </li>
           <br />
@@ -50,12 +51,12 @@ const Rules = () => {
               ROUND 3:
             </p>
             <br />
-            <p style={{ paddingLeft: '2.5rem' }}>
-            The final round will require the participants to replicate an entire website using HTML/CSS. The duration for this round 
-            will be 24 hours, by the end of which the participants should have a static website prepared that includes their own 
-            designs. <br />
-            
-            Platform: Discord <br />
+            <p style={{ paddingLeft: '2.5rem' , paddingBottom: ''}}>
+            The final round will require the participants to replicate an entire website using HTML/CSS. The duration 
+            for this round will be 24 hours, by the end of which the participants should have a static website prepared which 
+            includes their own designs. Theme will be revealed after round 2. <br />
+            <p style={{ paddingTop: '1.5rem'}}>
+            Platform: -- Discord <br /> </p>
             </p>
           </li>
         </ul>
